eb92954d1486eb752c839ee90e7b1404863caa15
ReyadBouzeboudja <bouzeboudja.reyad@gmail.com>
2022-02-25 16:12:27 +0100

    Mise à jours de l'organisation

--- emmad/Entity/Organisation.cs
+++ emmad/Entity/Organisation.cs
@@ -9,6 +9,8 @@ namespace emmad.Entity
         public int id { get; set; }
         public string adresse { get; set; }
         public int id_administrateur { get; set; }
+        public int nb_salarie { get; set; }
+        public string logo { get; set; }
         public Administrateur Administrateur { get; set; }
     }
 }
