6245fac8fa95f80b731ff2cb91ee947e9add1bd7
ReyadBouzeboudja <reyad.bouzeboudja@ynov.com>
2022-03-11 09:45:51 +0100

    Correction entity client

--- emmad/Entity/Client.cs
+++ emmad/Entity/Client.cs
@@ -5,8 +5,6 @@ namespace emmad.Entity
 {
     public class Client
     {
-        internal int id_administrateur;
-
         [Key]
         public int id { get; set; }
         public int id_organisation { get; set; }
